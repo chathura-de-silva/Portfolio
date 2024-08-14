0f5fa3c50cd87859f83505dbe765af201462dfe4
chathura-de-silva <ac.chathura>
2024-08-15 04:12:01 +0530

    Project section added

--- data/index.ts
+++ data/index.ts
@@ -50,7 +50,7 @@ export const navItems = [
     {
       id: 5,
       title: "I always prioritize the visual aspect of my projects just as much as their functionality.",
-      description: "Form or Functionality?  Nah, Form with Functionality...",
+      description: "Form or Functionality?",
       className: "md:col-span-3 md:row-span-2",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
       titleClassName: "justify-center md:justify-start lg:justify-center",
@@ -66,6 +66,41 @@ export const navItems = [
       titleClassName: "justify-center md:max-w-full max-w-60 text-center",
       img: "",
       spareImg: "",
+    },
+  ];
+  
+  export const projects = [
+    {
+      id: 1,
+      title: "Smart Medicine Box",
+      des: "A smart IoT device with an intuitive UI/UX that reminds users to take their medicine on time.",
+      img: "/p1.png",
+      iconLists: ["/platformio.svg", "/node-red.png", "/cpp.png", "/wokwi.png", "/arduino.png"],
+      link: "https://github.com/chathura-de-silva/Smart-Medibox",
+    },
+    {
+      id: 2,
+      title: "AcademEase",
+      des: "A Multiplatform University Workload Management Application Frontend with Material You",
+      img: "/p2.png",
+      iconLists: ["/dart.png", "/flutter.png", "/material3.png", "/androidstudio.svg", "/firestore.png"],
+      link: "https://this_one_is_closed_source🤭",
+    },
+    {
+      id: 3,
+      title: "Single Vendor E-Commerce Platform",
+      des: "Uses Server side rendering with flask micro-framework, focused on database design and security",
+      img: "/p3.png",
+      iconLists: ["/python.png", "/flask.png", "/mysql.svg", "/js.png", "/css.png"],
+      link: "https://github.com/chathura-de-silva/E-Commerce-Platform",
+    },
+    {
+      id: 4,
+      title: "Barnsley Fractal",
+      des: "Highly configurable visualization of the Barnsley Fern fractal implemented in both Pythona and Julia.",
+      img: "/p4.png",
+      iconLists: ["/python.png", "/vscode.png", "/julia.png", "/turtle.png", "/makie.svg"],
+      link: "https://github.com/chathura-de-silva/Barnsley-Fractal",
     },
   ];
   
