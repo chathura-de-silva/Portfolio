b62e3725c6a2fc3ad3d81fb55ad8e53496990a45
chathura-de-silva <ac.chathura>
2024-08-15 14:26:19 +0530

    Work Experience section customised to add an About Me.

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -6,8 +6,8 @@ const Clients = () => {
   return (
     <div className="py-20" id="testimonials">
       <h1 className="heading">
-        Kind words from{" "}
-        <span className="text-purple"> people who worked with me</span>
+        Kind Words From{" "}
+        <span className="text-purple"> People Who Worked With Me</span>
       </h1>
       <div className="flex flex-col items-center max-lg:mt-10"> 
           <InfiniteMovingCards
--- data/index.ts
+++ data/index.ts
@@ -10,9 +10,9 @@ export const navItems = [
 export const gridItems = [
   {
     id: 1,
-    title: "Technology & Nature",
+    title: "Defining My Essence",
     description:
-      "I’m passionate about both Technology and Nature, and I love merging them in my work.",
+      "A critical thinker with a keen eye for detail, I’m skilled at planning and executing with precision.",
     className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
     imgClassName: "w-full h-full",
     titleClassName: "justify-end",
@@ -199,29 +199,29 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "CSE Undergraduate",
+    desc: "B.sc.(Eng) Computer Science & Engineering - Specializing in Integrated Computer Engineering.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
   {
     id: 2,
-    title: "Mobile App Dev - JSM Tech",
-    desc: "Designed and developed mobile app for both iOS & Android platforms using React Native.",
+    title: "Math tutor",
+    desc: "helping students grasp math concepts and develop strong problem-solving skills.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp2.svg",
   },
   {
     id: 3,
-    title: "Freelance App Dev Project",
-    desc: "Led the dev of a mobile app for a client, from initial concept to deployment on app stores.",
+    title: "Dendrophile",
+    desc: "I love spending time in natural environments and scientific study of both flora & fauna.",
     className: "md:col-span-2", // change to md:col-span-2
     thumbnail: "/exp3.svg",
   },
   {
     id: 4,
-    title: "Lead Frontend Developer",
-    desc: "Developed and maintained user-facing features using modern frontend technologies.",
+    title: "Musician",
+    desc: "I play the violin and the piano, and I love to compose music in my free time.",
     className: "md:col-span-2",
     thumbnail: "/exp4.svg",
   },
